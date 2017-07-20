e8117a506dc2673c967a2b046b5db8e05286f34b
r.ranganathan <r.ranganathan@lakeba.com>
2017-07-20 18:31:26 +0530

    Added Instrumentation test classes

---
+++ app/src/androidTest/java/com/rramprasad/testingsample/view/LoginFragmentTest.java
@@ -0,0 +1,21 @@
+package com.rramprasad.testingsample.view;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Created by Lakeba
+ */
+public class LoginFragmentTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onClick() throws Exception {
+    }
+
+}
